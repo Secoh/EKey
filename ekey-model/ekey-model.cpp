667b6cabef7983b130025488e1ff5a097bfda32d
Secoh <oalferov1@gmail.com>
2021-09-14 00:46:38 -0700

    Initial commit

---
+++ ekey-model/ekey-model.cpp
@@ -0,0 +1,17 @@
+// This file is part of E-Key project - hardware/software platform to support certain cryptography applications.
+// Copyright [2021] Secoh  https://github.com/Secoh/EKey
+// This application uses SKLib: https://github.com/Secoh/SKLib
+//
+// Licensed under the GNU General Public License, Version 3 or later. See: https://www.gnu.org/licenses/
+// You may not use this file except in compliance with the License. Any derivative work must retain the License.
+// Software is distributed on "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+
+#include <iostream>
+
+#include <SKLib/sklib.hpp>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
